911c87a754f4d6d2ca5acb583805958c67b243f4
Renê Antunes Ladeira <93138911+Rene-Antunes@users.noreply.github.com>
2023-06-19 09:22:47 -0300

    Implementado filtro em consulta de vendas diárias

--- deliveryFood-API/src/main/java/com/reneantunes/reneFood/infrainstructure/service/VendaQueryServiceImpl.java
+++ deliveryFood-API/src/main/java/com/reneantunes/reneFood/infrainstructure/service/VendaQueryServiceImpl.java
@@ -1,10 +1,12 @@
 package com.reneantunes.reneFood.infrainstructure.service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.Predicate;
 
 import org.springframework.stereotype.Repository;
 
@@ -24,6 +26,8 @@ public class VendaQueryServiceImpl implements VendaQueryService {
 		var builder = manager.getCriteriaBuilder();
 		var query = builder.createQuery(VendaDiaria.class);
 		var root = query.from(Pedido.class);
+		var predicates = new ArrayList<Predicate>();
+		
 		
 		var functionDataCriacao = builder.function
 				("date", Date.class, root.get("dataCriacao"));
@@ -32,7 +36,24 @@ public class VendaQueryServiceImpl implements VendaQueryService {
 				functionDataCriacao, builder.count(root.get("id")),
 				builder.sum(root.get("valorTotal")));
 		
+		if(filtro.getRestauranteId() !=null) {
+			predicates.add(builder.equal(root.get("restaurante"), filtro.getRestauranteId()));
+		}
+		
+		if (filtro.getDataCriacaoInicio() != null) {
+			predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"), 
+					filtro.getDataCriacaoInicio()));
+		}
+		
+		if (filtro.getDataCriacaoFim() != null) {
+			predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"), 
+					filtro.getDataCriacaoFim()));
+		}
+		
+		
+		
 		query.select(selection);
+		query.where(predicates.toArray(new Predicate[0]));
 		query.groupBy(functionDataCriacao);
 		
 		return manager.createQuery(query).getResultList();
