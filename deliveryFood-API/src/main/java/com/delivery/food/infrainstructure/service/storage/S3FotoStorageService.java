f8ce92c55b8951bd9864b74fed39add15deee075
Renê Antunes Ladeira <93138911+Rene-Antunes@users.noreply.github.com>
2023-11-24 18:04:41 -0300

    feat: Implementado inclusão e exclusão de fotos no bucket da Amazon S3

--- deliveryFood-API/src/main/java/com/delivery/food/infrainstructure/service/storage/S3FotoStorageService.java
+++ deliveryFood-API/src/main/java/com/delivery/food/infrainstructure/service/storage/S3FotoStorageService.java
@@ -2,24 +2,63 @@ package com.delivery.food.infrainstructure.service.storage;
 
 import java.io.InputStream;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.delivery.food.core.storage.StorageProperties;
 import com.delivery.food.domain.service.FotoStorageService;
 @Service
 public class S3FotoStorageService implements FotoStorageService {
-
+	
+	@Autowired
 	private AmazonS3 amazonS3;
 	
+	@Autowired
+	private StorageProperties storageProperties;
+	
 	@Override
-	public void armazenar(NovaFoto novoFato) {
-		// TODO Auto-generated method stub
+	public void armazenar(NovaFoto novaFato) { 
+		try {
+	String caminhoArquivo = getCaminhoArquivo(novaFato.getNomeArquivo());
+	
+	var objectMetaData = new ObjectMetadata();
+	objectMetaData.setContentType(novaFato.getContetType());
+	
+		var putObjectRequest = new PutObjectRequest(
+				storageProperties.getS3().getBucket(),
+				caminhoArquivo,
+				novaFato.getInputStream(),
+				objectMetaData)
+				.withCannedAcl(CannedAccessControlList.PublicRead);
 		
+		amazonS3.putObject(putObjectRequest);
+		}catch (Exception e) {
+			throw new StorageException("Não foi possível enviar arquivo para Amazon S3.", e);
+		}
+	}
+
+	private String getCaminhoArquivo(String nomeArquivo) {
+		return String.format("%s/%s", storageProperties.getS3().getDiretorioFotos(), nomeArquivo);
 	}
 
 	@Override
 	public void remover(String nomeArquivo) {
-		// TODO Auto-generated method stub
+		try {
+			String caminhoArquivo = getCaminhoArquivo(nomeArquivo);
+			
+			var deleteObjectRequest = new DeleteObjectRequest(
+					storageProperties.getS3().getBucket(), caminhoArquivo);
+			
+			amazonS3.deleteObject(deleteObjectRequest);
+			
+		} catch (Exception e) {
+			 throw new StorageException("Não foi possível excluir arquivo na Amazon S3.", e);
+		}
 		
 	}
 
